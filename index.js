dab9bc8b03996987f09b26b6f13edc0e52cdfbe3
Nicolas <nicolasnb@icloud.com>
2018-08-03 14:21:39 +0100

    json-request

--- index.js
+++ index.js
@@ -26,17 +26,63 @@ const server = http.createServer((req,res) => {
 	var buffer= '';
 	req.on('data', data => {
 		buffer += decoder.write(data);
-	})
+	});
 	req.on('end', () => {
 		buffer += decoder.end();
-		// response
-		res.end('Hello World\n');
-		
-		// log request
-		console.log('payload = ', buffer);
-	})
+
+		// choose the handler this request should go to.if one is not found use notfound handler
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+		//construct the data object to send to the handler
+		var data = {
+			'trimmedPath' : trimmedPath,
+			'queryStringObject' : queryStringObject,
+			'method' : method,
+			'headers' : headers,
+			'payload' : buffer
+		};
+
+		// route the request to the handler specified in the router
+		chosenHandler(data, (statusCode, payload) => {
+			// use the status code called back by the handler, or default to 200
+			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+			// use the payload called back by the handler, or default to an empty object
+			payload = typeof(payload) == 'object' ? payload : {};
+
+			// convert the payload to a string
+			var payloadString = JSON.stringify(payload);
+
+			//return the response
+      res.setHeader('Content-Type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			// log request
+			console.log('Returning this response = ', statusCode, payloadString);
+		});
+	});
 });
 
 server.listen(3000, () => {
 	console.log('server is now listen port 3000');
 });
+
+//define the handlers
+var handlers = {};
+
+// sample handler
+handlers.sample = (data, callback) => {
+	// callback a http status code, and a payload object
+	callback(406, {'name' : 'sample handler'});
+};
+
+//not found handler
+handlers.notFound = (data, callback) => {
+	callback(404);
+}
+
+// define a quest router
+var router = {
+	'sample': handlers.sample
+};
